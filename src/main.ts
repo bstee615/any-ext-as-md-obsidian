837a24006387a94b763f7d265a2877c477fea195
Benjamin Steenhoek <benjaminjsteenhoek@gmail.com>
2022-04-06 10:06:27 -0500

    Add SampleSettingTab from sample plugin https://github.com/obsidianmd/obsidian-sample-plugin/blob/master/main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,60 @@
-import { Plugin } from 'obsidian';
+import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+
+interface MyPluginSettings {
+	mySetting: string;
+}
+
+const DEFAULT_SETTINGS: MyPluginSettings = {
+	mySetting: 'default'
+}
 
 export default class MdxAsMdPlugin extends Plugin {
+	settings: MyPluginSettings;
 
   async onload() {
     super.onload();
 
+		// This adds a settings tab so the user can configure various aspects of the plugin
+		this.addSettingTab(new SampleSettingTab(this.app, this));
+
     // register the view and extensions
     this.registerExtensions(["rmd"], "markdown");
   }
+
+	async loadSettings() {
+		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+	}
+
+	async saveSettings() {
+		await this.saveData(this.settings);
+	}
+}
+
+class SampleSettingTab extends PluginSettingTab {
+	plugin: MdxAsMdPlugin;
+
+	constructor(app: App, plugin: MdxAsMdPlugin) {
+		super(app, plugin);
+		this.plugin = plugin;
+	}
+
+	display(): void {
+		const {containerEl} = this;
+
+		containerEl.empty();
+
+		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
+
+		new Setting(containerEl)
+			.setName('Setting #1')
+			.setDesc('It\'s a secret')
+			.addText(text => text
+				.setPlaceholder('Enter your secret')
+				.setValue(this.plugin.settings.mySetting)
+				.onChange(async (value) => {
+					console.log('Secret: ' + value);
+					this.plugin.settings.mySetting = value;
+					await this.plugin.saveSettings();
+				}));
+	}
 }
