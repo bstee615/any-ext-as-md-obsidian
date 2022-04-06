e05404be3549b2d616820ebb0f95f5f357a245a7
Benjamin Steenhoek <benjaminjsteenhoek@gmail.com>
2022-04-06 10:40:36 -0500

    Use save button instead of live reload

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,27 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, ButtonComponent, Plugin, PluginSettingTab, Setting } from 'obsidian';
 
 interface MyPluginSettings {
 	mySetting: string;
 }
 
 const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+	mySetting: ''
 }
 
 export default class MdxAsMdPlugin extends Plugin {
 	settings: MyPluginSettings;
+  currentlyRegistered: Set<string> = new Set<string>();
 
   async onload() {
     super.onload();
+    
+		await this.loadSettings();
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new SampleSettingTab(this.app, this));
 
     // register the view and extensions
-    this.registerExtensions(["rmd"], "markdown");
+    this.doRegisterExtensions();
   }
 
 	async loadSettings() {
@@ -28,6 +31,20 @@ export default class MdxAsMdPlugin extends Plugin {
 	async saveSettings() {
 		await this.saveData(this.settings);
 	}
+
+  doRegisterExtensions() {
+    let extensions = this.settings.mySetting.split(",").map(e => e.trim());
+    let newExtensions = extensions.filter(e => !this.currentlyRegistered.has(e));
+    this.registerExtensions(newExtensions, "markdown");
+    extensions.forEach(e => this.currentlyRegistered.add(e));
+    console.log("Registered new extensions:", newExtensions, "All registered extensions:", this.currentlyRegistered);
+  }
+
+  get currentlyRegisteredPretty() {
+    let crList = Array.from(this.currentlyRegistered);
+    crList.sort();
+    return crList.join(", ");
+  }
 }
 
 class SampleSettingTab extends PluginSettingTab {
@@ -45,16 +62,27 @@ class SampleSettingTab extends PluginSettingTab {
 
 		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
 
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
+		let extensionsInput = new Setting(containerEl)
+			.setName('File extensions to recognize as Markdown')
+			.setDesc('Comma-delimited (e.g. "txt,mdx,rmd"). Currently registered extensions: ' + this.plugin.currentlyRegisteredPretty)
 			.addText(text => text
-				.setPlaceholder('Enter your secret')
+				.setPlaceholder('Enter your Markdown file extensions')
 				.setValue(this.plugin.settings.mySetting)
 				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+          this.plugin.settings.mySetting = value
+        }));
+        
+		new Setting(containerEl)
+      .setName('Save file extensions')
+      .setDesc('Save and register the current set of file extensions.')
+      .addButton((button: ButtonComponent) => {
+        button.setButtonText('Save');
+        button.onClick(async () => {
+          await this.plugin.saveSettings();
+          await this.plugin.doRegisterExtensions();
+          this.display();
+        });
+        button.setCta();
+      });
 	}
 }
